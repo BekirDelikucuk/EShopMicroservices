dee5140b52583ca0c0b2ccd9a45fc1848a6ba895
BekirDelikucuk <91602566+BekirDelikucuk@users.noreply.github.com>
2025-06-24 00:44:44 +0300

    Ordering.Application Layer with CQRS and MediaTR

--- src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
 using Ordering.Domain.Models;
 
 namespace Ordering.Infrastructure.Data;
 
-internal class ApplicationDbContext : DbContext
+internal class ApplicationDbContext : DbContext, IApplicationDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options) { }
