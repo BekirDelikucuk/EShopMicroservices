0e5706227312b03b0b542a0bc4d1f8fd84769749
BekirDelikucuk <91602566+BekirDelikucuk@users.noreply.github.com>
2025-11-25 23:34:13 +0300

    develop abscket checkout feature

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,5 @@
 using Discount.Grpc;
-
+using BuildingBlocks.Messaging.MassTransit;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -45,6 +45,9 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
 
         return handler;
     });
+
+//Async Communication Services
+builder.Services.AddMessageBroker(builder.Configuration);
 
 //Cross-cutting Services
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
